54f0c3ca88efc248564e00f9b1967ebd0458cdca
osamax2009 <osamafalah85@yahoo.com>
2025-07-08 21:36:02 +0300

    Update HomeScreen.kt

--- app/src/main/java/com/example/todoproject/screens/HomeScreen.kt
+++ app/src/main/java/com/example/todoproject/screens/HomeScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Refresh
@@ -50,6 +52,7 @@ fun HomeScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
